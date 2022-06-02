4405adcecea42809e8388033c6e242d8133bfe20
akos <akos@netcompany.com>
2022-06-02 14:33:32 +0200

    azure db

---
+++ API/Entities/Data/Booking/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities.Data.Booking
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "Firstname is required")]
+        public string? Firstname { get; set; }
+        [Required(ErrorMessage = "Lastname is required")]
+        public string? Lastname { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string MyProperty { get; set; }
+    }
+}
