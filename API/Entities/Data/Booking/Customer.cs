afb72fc03193b3c1548d9f4fb31b270c2af25363
akos <akos@netcompany.com>
2022-06-02 15:13:32 +0200

    database changses

--- API/Entities/Data/Booking/Customer.cs
+++ API/Entities/Data/Booking/Customer.cs
@@ -6,10 +6,17 @@ namespace API.Entities.Data.Booking
     {
         [Required(ErrorMessage = "Firstname is required")]
         public string? Firstname { get; set; }
+
         [Required(ErrorMessage = "Lastname is required")]
         public string? Lastname { get; set; }
 
+        [Required(ErrorMessage = "Address is required")]
+        public string? Address { get; set; }
+
         [Required(ErrorMessage = "Email is required")]
-        public string MyProperty { get; set; }
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Phonenumber is required")]
+        public string? Phonenumber { get; set; }
     }
 }
