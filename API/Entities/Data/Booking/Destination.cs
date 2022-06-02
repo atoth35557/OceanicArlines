b828942dc26f126d3c603b05d3d44538ee3abcab
LarsGS <Larsgs@live.dk>
2022-06-02 15:12:55 +0200

    Adding entities, destination and packages

---
+++ API/Entities/Data/Booking/Destination.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities.Data.Booking
+{
+    public class Destination
+    {
+        [Required(ErrorMessage = "From Destination is required")]
+        public string? From { get; set; }
+        [Required(ErrorMessage = "To Destination is required")]
+        public string? To { get; set; }
+
+    }
+}
